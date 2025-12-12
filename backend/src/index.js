8abf64990839542acf8f0bb8c4ef0d7130fbe7a3
Yousfe Ahmed Maher <ya1770620@gmail.com>
2025-12-12 04:04:13 +0200

    add all

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.delete('/api/items/:id', deleteItem);
 
 db.init()
     .then(() => {
-        app.listen(3000, () => console.log('Listening on port 3000'));
+        app.listen(3000, () => console.log('  Listening on port 3000'));
     })
     .catch((err) => {
         console.error(err);
